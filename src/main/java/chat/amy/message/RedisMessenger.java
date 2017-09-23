f6a662137dfa8c1cd0fb39fa8bc0918d852e2cc0
Amy Null <amy@willnot.date>
2017-09-23 17:56:07 -0400

    Initial READY guild caching

--- src/main/java/chat/amy/message/RedisMessenger.java
+++ src/main/java/chat/amy/message/RedisMessenger.java
@@ -1,19 +1,30 @@
 package chat.amy.message;
 
 import chat.amy.jda.WrappedEvent;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.redisson.Redisson;
 import org.redisson.api.RBlockingQueue;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 /**
  * @author amy
  * @since 9/22/17.
  */
+@SuppressWarnings("unused")
 public class RedisMessenger implements EventMessenger {
     private final RedissonClient redis;
+    
+    private final List<WrappedEvent> preloadEventCache = new ArrayList<>();
+    private final List<String> streamableGuilds = new ArrayList<>();
+    private int streamedGuildCount = 0;
+    private boolean isStreamingGuilds = true;
     
     public RedisMessenger() {
         final Config config = new Config();
@@ -28,6 +39,36 @@ public class RedisMessenger implements EventMessenger {
     
     @Override
     public void queue(final WrappedEvent event) {
+        // So this is actually a bit interesting. We need to not ship off events until we finish streaming all the guilds,
+        // because otherwise the backend might not have caches available etc.
+        // This is solved by
+        // - Get READY event
+        
+        if(event.getType().equalsIgnoreCase("READY")) {
+            // Discord READY event. Cache unavailable guilds, then start streaming
+            final JSONArray guilds = event.getData().getJSONArray("guilds");
+            StreamSupport.stream(guilds.spliterator(), false)
+                    .map(JSONObject.class::cast).map(o -> o.getString("id")).forEach(streamableGuilds::add);
+        } else if(event.getType().equalsIgnoreCase("GUILD_CREATE")) {
+            // Cache the guild
+            // TODO: Redisson cache
+            if(isStreamingGuilds) {
+                final String id = event.getData().getString("id");
+                // If we're streaming guilds, we need to make sure we mark "finished" when we're done
+                if(streamableGuilds.contains(id)) {
+                    ++streamedGuildCount;
+                    if(streamedGuildCount == streamableGuilds.size()) {
+                        isStreamingGuilds = false;
+                        preloadEventCache.forEach(this::queue);
+                    }
+                }
+            }
+        }
+        if(isStreamingGuilds) {
+            preloadEventCache.add(event);
+            return;
+        }
+        
         final RBlockingQueue<WrappedEvent> eventQueue = redis.getBlockingQueue("discord-intake");
         try {
             eventQueue.add(event);
